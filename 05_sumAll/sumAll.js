a277625d46f401a22873b3877012878718ed42b7
Jacksongee <jacksonbgee@gmail.com>
2022-10-05 09:41:46 -0400

    Add exercise05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numOne,numTwo) {
+    if (typeof numOne != 'number' || typeof numTwo != 'number')
+        return 'ERROR';
+    if (numOne <0 || numTwo <0)
+        return 'ERROR';
+    
+    if (numOne < numTwo){
+        loopStart = numOne;
+        loopEnd = numTwo;
+    }   
+    else{
+        loopStart = numTwo;
+        loopEnd = numOne;
+    }
+    sum = 0;
+    for (i = loopStart;i <= loopEnd; i ++)
+        sum += i;
 
+    return sum;
 };
 
 // Do not edit below this line
