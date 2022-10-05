e69b690f492a7b549ff2dab9e62334a6aa13fd83
Jacksongee <jacksonbgee@gmail.com>
2022-10-05 01:08:16 -0400

    Add exercise04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    numOfArguements = arguments.length;
+    list = arguments[0]
+    for (i=1;i<numOfArguements;i++){
+        for (argument of list)
+            if (arguments[i]===argument)
+                list.splice(list.indexOf(argument),1)
+    }
+    return list;
 };
 
 // Do not edit below this line
