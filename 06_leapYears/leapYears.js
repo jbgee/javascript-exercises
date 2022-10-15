adb2aedafc4b2ae795ae83d58b5dbf8aa54a165c
Jacksongee <jacksonbgee@gmail.com>
2022-10-15 00:16:58 -0400

    Add exercise 06 and 07 solutions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year != 'number')
+        return false;
+    
+    if (year % 4 == 0){
+        if (year % 100 == 0){
+            if(year % 400 == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  if (typeof farenheit != 'number')
+    return 'ERROR'
+  let celcius = (farenheit - 32) *5 /9;
+  let roundedCelcius = Math.round(celcius*10)/10;
+  return roundedCelcius;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  if (typeof celcius != 'number')
+    return 'ERROR'
+  let farenheit = (celcius*9/5)+32;
+  let roundedFarenheit = Math.round(farenheit*10)/10;
+  return roundedFarenheit;
 };
 
 // Do not edit below this line
