60a6372901cab3575970af64c316fbbe912843f0
Jacksongee <jacksonbgee@gmail.com>
2022-10-15 00:16:16 -0400

    Add exercise 08 solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
 const add = function() {
-	
+  return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+  console.log(Array.from(arguments));
+	return arguments[0].reduce((sum,number) => {return sum + number},0);
 };
 
 const multiply = function() {
-
+  return arguments[0].reduce((product,number) => {return product * number},1)
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	num = arguments[0];
+  product = 1;
+  while(num >0){
+    product *= num;
+    num--;
+  }
+  return product;
 };
 
 // Do not edit below this line
