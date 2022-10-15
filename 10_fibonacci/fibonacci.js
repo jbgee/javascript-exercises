b6787f2c04db73ef35ae83ed5b4370656841bde9
Jacksongee <jacksonbgee@gmail.com>
2022-10-15 11:26:52 -0400

    Add exercise 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
 const fibonacci = function() {
+    let fibonacci = [1,1];
+    const index = Number(arguments[0]);
+    if (index < 0){
+        return "OOPS";
+    }
+    else {
+        let sequenceLength = fibonacci.length;
+        while(fibonacci.length <= index){
+            sequenceLength = fibonacci.length;
 
+            fibonacci.push(fibonacci[sequenceLength-1]+fibonacci[sequenceLength-2]);
+        }
+        return fibonacci[sequenceLength-1];
+    }
+    
 };
 
 // Do not edit below this line
