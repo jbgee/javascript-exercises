b38c62f670b53ac0cb642a3e0d4badbe55576cf8
Jacksongee <jacksonbgee@gmail.com>
2022-10-15 10:07:33 -0400

    Add exercise 09 solution. Need to be very careful to write the correct syntax

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
 const palindromes = function () {
-
+    const totalText = arguments[0];
+    const alphaText = totalText.split("").filter(letter => {
+        return /^[a-z]$/i.test(letter.toLowerCase());
+    });
+    stringLength = alphaText.length;
+    halfLength = Math.floor(stringLength/2);
+    for(let i= 0; i<stringLength;i++){
+        if(alphaText[i].toLowerCase()!==alphaText[stringLength-1-i].toLowerCase())
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
